0d3319dee10d70ca965d3cfb57d997dd83522847
Yasmin <yasmin@lit.ifce.edu.br>
2019-12-20 08:49:44 -0300

    #25 migrations to mysql

--- database/migrations/2019_12_16_000447_create_reports_table.php
+++ database/migrations/2019_12_16_000447_create_reports_table.php
@@ -21,12 +21,14 @@ class CreateReportsTable extends Migration
             $table->string('logoCompanyContracting');
 
             $table->integer('user_id')->unsigned();
+
             $table->foreign('user_id')
                     ->references('id')->on('users')
                     ->onDelete('cascade');
 
             $table->timestamps();
         });
+
     }
 
     /**
